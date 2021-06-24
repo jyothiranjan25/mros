9e41d47c7be214e3e679697bc2a5d7b8dd8931bd
Rahul Krish <asrahulkrishna@gmail.com>
2021-06-24 15:03:21 +0530

    config update

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,7 +1,8 @@
 
 
 <?php
-$con=mysqli_connect("localhost", "root", "", "mros");
+$con=mysqli_connect("localhost", "vuuniversity", "^&*GuYtJH&*49R&^", "mros");
+// $con=mysqli_connect("localhost", "root", "", "mros");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
